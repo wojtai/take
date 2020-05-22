bf3af784a703626ee74f69968df6c42027a0d94d
wojtek <wojtas.t.1997@gmail.com>
2020-05-22 12:32:24 +0200

    rest api ajax

---
+++ Lab-10/WebApiAjax/WebApiAjax/Controllers/MathController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAjax.Models;
+
+namespace WebApiAjax.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET api/math
+        [HttpGet]
+        public CalcResult Calculate(int x, int y)
+        {
+            return new CalcResult()
+            {
+                Sum = x + y,
+                Difference = x - y,
+                Product = x * y,
+                Quotient = x / y
+            };
+        }
+    }
+}
